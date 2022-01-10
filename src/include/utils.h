9da5a47f0d5ac499c73dd07b80b2d5a3632dff61
박신석 <hotstone1993@gmail.com>
2022-01-11 03:20:11 +0900

    initial commit

---
+++ src/include/utils.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_UTILS_H
+#define MAIN_UTILS_H
+
+enum {
+    SUCCESS = 0,
+    FAIL = -1
+};
+
+#endif
---
+++ tools/scripts/build.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+
+BUILD_DIR = '../../build'
+
+if not os.path.isdir(BUILD_DIR):
+    os.mkdir(BUILD_DIR)
+os.chdir(BUILD_DIR)
+subprocess.call(['cmake', '..'])
